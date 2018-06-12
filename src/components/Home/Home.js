c6ff7c36b8533804660e3e170678fe4b5983a699
midhun1001 <midhun.murali.mm@gmail.com>
2018-06-12 18:38:07 +0530

    api call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import styles from './Home.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => (
-  <div className={styles.home}>
-    Home page
-  </div>
-);
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        if (response.data) {
+          this.setState({ users: response.data });
+        }
+      });
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.state.users.length > 0 &&
+            this.state.users.map((val, index) => (
+              <li key={index}>{val.name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Home;
